c4223654d56bd0b655cb22e4493c5f7f0c7f9e80
refire99 <faermaps@gmail.com>
2025-05-06 14:15:55 +0300

    update2

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ def test_add_to_basket_button_is_displayed(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     
-    time.sleep(10) 
+    time.sleep(30) 
     
     add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, "button.btn-add-to-basket")
     assert len(add_to_basket_button) > 0, "Button 'Add to basket' is not displayed!"
